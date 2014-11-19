933c2384b675e07a9152151c4532a5397a1424a6
XiongwenYu <akongxiong@hotmail.com>
2014-11-19 22:34:47 +0800

    提交解决方案

---
+++ AKong.FrameWork.Data/OAContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AKong.FrameWork.Data.Models;
+
+namespace AKong.FrameWork.Data
+{
+    public class OaContext : DbContext
+    {
+        public OaContext()
+            : base("name=OA")
+        {
+        }
+        public IDbSet<User> Users { get; set; }
+    }
+}
